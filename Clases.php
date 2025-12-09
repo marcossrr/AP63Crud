9dd26039114ca684bdf284d1802c3b0961a8aaf0
Marcos <mariro@alumnatflorida.es>
2025-12-09 12:49:20 +0100

    Creo la clase padre

---
+++ Clases.php
@@ -0,0 +1,35 @@
+<?php
+
+Class Consolas {
+    private $id;
+    private $nombre;
+    private $precio;
+
+    public function __construct($id, $nombre, $precio){
+        $this->id = $id;
+        $this->nombre = $nombre;
+        $this->precio = $precio;
+    }
+
+    getId(){
+        return $this->id;
+    }
+    getNombre(){
+        return $this->nombre;
+    }
+    getPrecio(){
+        return $this->precio;
+    }
+
+    setId(){
+        $this->id = $id;
+    }
+    setNombre(){
+        $this->nombre = $nombre;
+    }
+    setPrecio(){
+        $this->precio = $precio;
+    }
+}
+
+?>
